c6b26b17a07aa25035d6e5f12daf45e72d512eb2
boateng-dev boatenganthony78@gmail.com <boateng-dev.boatnganthony78@gmail.com>
2026-03-12 06:23:29 +0000

    week1/Excercise2/program.cs

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,42 @@
 using System;
 
-class Program
+class GradeCalculator
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.Write("Enter your grade percentage: ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, you try again next time");
+        }
     }
 }
