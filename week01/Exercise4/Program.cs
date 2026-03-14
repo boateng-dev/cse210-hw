38030744edbf5d9c00d801edb590424292e771ef
boateng-dev boatenganthony78@gmail.com <boateng-dev.boatnganthony78@gmail.com>
2026-03-14 04:30:48 +0000

    week01/Excercise4/Programe.cs

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        {
+            List<double> numbers = new List<double>();
+
+            Console.WriteLine("Enter a numbers, type 0 when ending.");
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+                if (number == 0)
+                    break;
+                numbers.Add(number);
+            }
+
+            double sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            double average = sum / numbers.Count;
+            double max = double.MinValue;
+            foreach (var number in numbers)
+            {
+                if (number > max)
+                    max = number;
+            }
+
+            Console.WriteLine($"The max is: {max}");
+        }
     }
 }
