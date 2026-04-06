b7c26ecf82a807bb75b84c29cdce4dc9ca27c31e
boateng-dev boatenganthony78@gmail.com <boateng-dev.boatnganthony78@gmail.com>
2026-04-06 00:15:59 +0000

    week05/Homework/Assignment.cs

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,55 @@
+public abstract class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    protected string GetStudentName()
+    {
+        return _studentName;
+    }
+}
+
+public class MathAssignment : Assignment
+{
+    private string _section;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string section, string problems) 
+        : base(studentName, topic)
+    {
+        _section = section;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_section} Problems {_problems}";
+    }
+}
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) 
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+         Assignment assignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(((MathAssignment)assignment).GetHomeworkList());
+
+        assignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(((WritingAssignment)assignment).GetWritingInformation());
     }
 }
